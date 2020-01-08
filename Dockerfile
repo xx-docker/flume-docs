c457e4e6a3382353d3c5b7ca3f32119a42e9fc7f
xx-zhang <zhangxiaoxiang@sulfilter.com>
2020-01-08 13:55:03 +0800

    :tada::whale:增加了flume1.8中文文档-Docker存储

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM registry.cn-hangzhou.aliyuncs.com/xxzhang/tengine
+
+ADD . /usr/share/nginx/html
---
+++ _static/stat.js
@@ -0,0 +1,10 @@
+var myVersion = 16 ;
+
+/** google begin */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-132785326-2'); 
+/** google end */
+
